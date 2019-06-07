3549c4ae3e5638dbd331cd9417f3607fabfee06e
Roman <smalldarkfilms@gmail.com>
2019-06-06 21:45:00 -0400

    Added php header and footer, and includes to index file

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+    <nav>
+            <ul>
+                <li class="current"><a href="about.html">About</a></li>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+
+        <footer>
+            <div>
+                <h4><a href="mailto:hello@romanmarr.com">hello@romanmarr.com</a></h4>
+            </div>
+            <div>
+                <h5> <a href="https://twitter.com/theromanpaul">Twitter</a> | <a href="https://www.linkedin.com/in/theromanpaul/">LinkedIn</a> | <a href="https://github.com/romanpaul">GitHub</a></h5>
+            </div>
+            <div>
+                <p>Coded with love and JavaScript | &copy 2019 Roman Marr</p>
+            </div>
+            
+        </footer>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ inc/header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+
+        <title>Roman Marr | Software Developer</title>
+        <meta name="description" content="">
+        <meta name="keywords" content=".Net, C#, Web Developer, Interactive Developer, Front End developer, Full Stack Developer, UI, UX, Portfolio, HTML5, CSS3，Sass, JavaScript, jQuery, Node.js, WordPress, Git">
+        <meta name="author" content="Roman Marr">
+        <meta name="viewport" content="width=device-width">
+        
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108384521-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-108384521-1');
+        </script>
+        
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Lora|Raleway:300, 400" rel="stylesheet">
+    </head>
+
+    <body>
+        
+        <div class="name">
+            <h3><a href="index.html">Roman Marr</a></h3>
+        </div>
